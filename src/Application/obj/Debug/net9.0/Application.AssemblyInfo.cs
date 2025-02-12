499663a1c499b4c6545502eda37c18d4ecb4836a
CtrlCVbot <ctrlcvmail@gmail.com>
2025-02-12 20:14:05 +0900

    작업 중인 변경 사항 커밋

--- src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+626d530e3dc7f32834b6d2eaac831e492dc74272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763dcd6b73e4e8d0246fe79be740ba50a9a7c678")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
